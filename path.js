2ed6f0c51b1fb73e9887a528b2d8ad7b7f117ef4
BouazziH <bouazzicdi@gmail.com>
2022-09-12 16:21:22 +0200

    afficher le non et l'extension d'un fichier et le dossier avec des function js

---
+++ path.js
@@ -0,0 +1,10 @@
+const path = require("path");
+//affichage du nom de repertoire
+
+console.log(path.dirname("C:\\Users\\Ceppic\\Documents\\nodejs\\path.js"));
+// affichage du nom de fichier
+
+console.log(path.basename("C:\\Users\\Ceppic\\Documents\\nodejs\\path.js"));
+// affichage de l'extension du ficher
+
+console.log(path.extname("C:\\Users\\Ceppic\\Documents\\nodejs\\path.js"));
