a156e766c73ce8d02ce259784dcfd6a330967792
BouazziH <bouazzicdi@gmail.com>
2022-09-12 14:37:38 +0200

    affiche en deux methode lire les commentaires

--- argument.js
+++ argument.js
@@ -1,2 +1,9 @@
 /* console.log(process.argv[2]); */
-console.log(process.argv.slice(2)[1].split('=',2));
+//slice et split des fonctions java  look at the docs 
+/* console.log(process.argv.slice(2)[0].split('=')[1]); */
+//deuxieme methode regarder le readme
+const minimist = require("minimist");
+const argNew = minimist(process.argv.slice(2));
+//pour afficher en bash on met node argument.js --nom pour l'affichage
+console.log(argNew.nom);
+console.log(argNew.prenom);
