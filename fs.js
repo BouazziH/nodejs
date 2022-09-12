880730cdcae4a4a97ebe24ae4e17ce266b320ae2
BouazziH <bouazzicdi@gmail.com>
2022-09-12 16:31:07 +0200

    lister tout les fichiers

--- fs.js
+++ fs.js
@@ -1,6 +1,11 @@
-const fs = require("fs");
+/* const fs = require("fs");
 const path = require("path");
 //comment lire les donnes de ficher json
 const fileName =  path.join(__dirname,'sample.json');
 fs.readFile(fileName,(err,contenue) => {console.log(String(contenue))
 })
+
+ */
+// pour lister tous les fichiers 
+const {readdir} = require("fs");
+readdir('.',(error,files) => console.log(files));
