79d0e8ee9983c603f80cb2647a22fa6ffaa3651f
BouazziH <bouazzicdi@gmail.com>
2022-09-13 11:17:37 +0200

    just to switch

--- serveur.js
+++ serveur.js
@@ -2,7 +2,7 @@ const http =  require("http");
 require('dotenv').config();
 
 //const port = 1309;
-const port = process.env.PORT || 1309;
+ const port = process.env.PORT || 1309;
 
 const serveur = http.createServer((req, res) =>
 {
@@ -11,4 +11,4 @@ res.setHeader('Contente-Type','text/html')
 res.end("<h1>Welcome to Bouazzi's server</h1>")
 })
 
-serveur.listen(port, () => {console.log(`serveur lancer à l'adresse http://127.0.0.1:${port}`)})
+serveur.listen(port, () => {console.log(`serveur lancer à l'adresse http://127.0.0.1:${port}`)}) 
