3e22f56ca69798a2ca2542359148f2aab04ed6cb
BouazziH <bouazzicdi@gmail.com>
2022-09-13 10:15:52 +0200

    faire la meme chose mais avec une variable declarer sur le .env RTM

--- serveur.js
+++ serveur.js
@@ -1,4 +1,5 @@
 const http =  require("http");
+require('dotenv').config();
 
 //const port = 1309;
 const port = process.env.PORT || 1309;
@@ -7,7 +8,7 @@ const serveur = http.createServer((req, res) =>
 {
 res.statusCode = 200
 res.setHeader('Contente-Type','text/html')
-res.end('<h1>Welcome to Bouazzis server</h1>')
+res.end("<h1>Welcome to Bouazzi's server</h1>")
 })
 
 serveur.listen(port, () => {console.log(`serveur lancer à l'adresse http://127.0.0.1:${port}`)})
