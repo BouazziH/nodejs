8036365c6963f883802c048bcf5736fa7bf62aa7
BouazziH <bouazzicdi@gmail.com>
2022-09-13 09:27:28 +0200

    ajout des fonctionalité pour afficherla version de node

---
+++ child_process.js
@@ -0,0 +1,8 @@
+const { exec } = require("child_process");
+const { stderr } = require("process");
+exec("npm --version", (error, stdout, stderr) => {console.log(`npm version ${stdout.trim()}`);
+});
+
+exec('node -v',(error, stdout, stderr) => {
+    console.log(`la version de note est ${stdout.trim()}`);
+});
